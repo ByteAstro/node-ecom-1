0ca5406e0f021467dd6913ba0f5e0f2a018193f6
Byte Astro <biswajitswain348@gmail.com>
2024-03-09 17:30:56 +0530

    userModel used

--- app.js
+++ app.js
@@ -16,28 +16,39 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// db.execute('SELECT * FROM products')
-//     .then(result => {
-//         console.log(result[0], result[1]);
-//     }).catch(err => {
-//         console.log(err);
-//     });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1);
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
-sequelize.sync()
+sequelize
+    // .sync({ force: true })
+    .sync()
     .then(result => {
+        return User.findByPk(1);
         // console.log(result);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Max', email: 'test@emai.com' });
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
         app.listen(3000);
-    }).catch(err => {
+    })
+    .catch(err => {
         console.log(err);
     });
 
