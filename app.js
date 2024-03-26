238368e742c955084fb4e2e2cea021d532bc4bc2
Byte Astro <biswajitswain348@gmail.com>
2024-03-26 18:14:34 +0530

    mongoose use -1

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require('./controllers/error.js');
-const mongoConnect = require('./util/database.js').mongoConnect;
+const errorController = require('./controllers/error');
 const User = require('./models/user.js');
 
 dotenv.config();
@@ -21,19 +21,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('65fd3d6dd05c3af62b1df7d7')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+	User.findById('6602bfd45fd4bf6e08f6bc8e')
+		.then(user => {
+			req.user = user;
+			next();
+		})
+		.catch(err => console.log(err));
 });
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect(process.env.MONGO_URI)
+	.then(result => {
+		console.log('MongooDB connected Successfully!');
+		User.findOne().then(user => {
+			if (!user) {
+				const user = new User({
+					name: 'sam',
+					email: 'sam31@xyz.com',
+					cart: {
+						items: []
+					}
+				});
+				user.save();
+			}
+		})
+		app.listen(3000);
+	}).catch(err => console.log(err));
--- util/database.js
+++ util/database.js
@@ -6,22 +6,21 @@ let _db;
 const mongoConnect = (callback) => {
   MongoClient.connect(process.env.MONGO_URI)
     .then(client => {
-      console.log('MongoDB Connected successfully!');
+      console.log('MongDB Connected successfully!');
       _db = client.db();
       callback();
     }).catch(err => {
       console.log(err);
       throw err;
-    })
+    });
 };
 
 const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw "No database found!";
+  throw 'Noo database found!';
 };
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
